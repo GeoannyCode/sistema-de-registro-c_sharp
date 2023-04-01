bfea9f58c71027a2e148007f36339a6a7073c5d1
diego <diego@DESKTOP-1PBCU0I>
2023-04-01 17:15:41 -0500

    Agregar archivos de proyecto.

---
+++ sistema-de-registro/Program.cs
@@ -0,0 +1,53 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] userName = new string[10] { "Diego", "", "", "", "", "", "", "", "", "" };
+
+        int arrayCurrent = 1;
+        string userType;
+
+        Console.WriteLine("*********** Welcome to the C# restaurant ***********");
+        while (arrayCurrent < 10)
+        {
+       
+            Console.WriteLine("\nBot: Are you registered user ?  Write 'yes', or write 'no' to register");
+            userType = Console.ReadLine();
+
+            switch (userType)
+            {
+                case "yes" or "y":
+                    Console.WriteLine("Hello, you are a registered user, please enter your exact username");
+                    string userToSearch = Console.ReadLine();
+                    Console.WriteLine("The user you selected is: " + userToSearch);
+
+                    int index = Array.IndexOf(userName, userToSearch);
+                    if (index == -1)
+                    {
+                        Console.WriteLine("!!!User not found, try again or register");
+
+                    }
+                    else
+                    {
+                        Console.Write("Welcome {0}, it's a pleasure to give you food. \n \n", userName[index]);
+                    }
+                break;
+                case "no" or "n":
+                    Console.WriteLine("Please write your username ");
+                    userName[arrayCurrent] = Console.ReadLine();
+                    Console.WriteLine("Your user has been saved succesfull \n"
+                         + "Your User Name is: {0}", userName[arrayCurrent]);
+                    arrayCurrent++;
+                break;
+            }
+        }
+        Console.WriteLine("The restaurant is full, try again next year.");
+        int auxIndex = 0;
+        foreach (string element in userName)
+        {
+            Console.WriteLine("User number: {0} and username: {1}.", auxIndex + 1, element);
+            auxIndex++;
+        } ;
+        Environment.Exit(0);
+    }
+}
